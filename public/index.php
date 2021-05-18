21ecdf202aa7dbff896ece1495cbd875d2262291
equesada <equesada@ludonkey.com>
2021-05-18 09:41:49 +0200

    Move into the switch the display/default case

--- public/index.php
+++ public/index.php
@@ -17,29 +17,6 @@ $codeRepo = $orm->getRepository(Code::class);
 $userRepo = $orm->getRepository(User::class);
 $langRepo = $orm->getRepository(Language::class);
 
-$codes = [];
-
-if (isset($_GET["search"])) {
-    $strToSearch = $_GET["search"];
-    $posOfTheAtCharacter = strpos($strToSearch, "@");
-
-    if ($posOfTheAtCharacter === 0) {
-        $userName = substr($strToSearch, 1);
-        $users = $userRepo->findBy(array("nickname" => $userName));
-        if (count($users) == 1) {
-            $codes = $codeRepo->findBy(array("user" => $users[0]->id));
-        }
-    } else {
-        $codes = $codeRepo->findBy(
-            array("desc" => "%$strToSearch%")
-        );
-    }
-} else {
-    $codes = $codeRepo->findAll();
-}
-
-include "../templates/display.php";
-
 $action = $_GET["action"] ?? "display";
 switch ($action) {
     case 'register':
@@ -52,5 +29,25 @@ switch ($action) {
         break;
     case 'display':
     default:
+        $codes = [];
+        if (isset($_GET["search"])) {
+            $strToSearch = $_GET["search"];
+            $posOfTheAtCharacter = strpos($strToSearch, "@");
+
+            if ($posOfTheAtCharacter === 0) {
+                $userName = substr($strToSearch, 1);
+                $users = $userRepo->findBy(array("nickname" => $userName));
+                if (count($users) == 1) {
+                    $codes = $codeRepo->findBy(array("user" => $users[0]->id));
+                }
+            } else {
+                $codes = $codeRepo->findBy(
+                    array("desc" => "%$strToSearch%")
+                );
+            }
+        } else {
+            $codes = $codeRepo->findAll();
+        }
+        include "../templates/display.php";
         break;
 }
