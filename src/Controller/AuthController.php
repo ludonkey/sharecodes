74764304b1868502cc98523426347994821b7bdb
equesada <equesada@ludonkey.com>
2021-05-25 11:22:54 +0200

    Migration Register to AuthController

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace Controller;
 
+use Entity\User;
+
 class AuthController
 {
     public function login()
@@ -31,5 +33,41 @@ class AuthController
             unset($_SESSION['user']);
         }
         header('Location: ?action=display');
+    }
+
+    public function register()
+    {
+        global $userRepo;
+        global $manager;
+
+        if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['passwordRetype'])) {
+            $errorMsg = NULL;
+            $criteriaWithlogin = [
+                "nickname" => $_POST['username']
+            ];
+            $usersWithThisNickname = $userRepo->findBy($criteriaWithlogin);
+            if (count($usersWithThisNickname) > 0) {
+                $errorMsg = "Nickname already used.";
+            } else if ($_POST['password'] != $_POST['passwordRetype']) {
+                $errorMsg = "Passwords are not the same.";
+            } else if (strlen(trim($_POST['password'])) < 8) {
+                $errorMsg = "Your password should have at least 8 characters.";
+            } else if (strlen(trim($_POST['username'])) < 4) {
+                $errorMsg = "Your nickame should have at least 4 characters.";
+            }
+            if ($errorMsg) {
+                include "../templates/RegisterForm.php";
+            } else {
+                $newUser = new User();
+                $newUser->nickname = $_POST['username'];
+                $newUser->password = $_POST['password'];
+                $manager->persist($newUser);
+                $manager->flush();
+                $_SESSION['user'] = $newUser;
+                header('Location: ?action=display');
+            }
+        } else {
+            include "../templates/RegisterForm.php";
+        }
     }
 }
